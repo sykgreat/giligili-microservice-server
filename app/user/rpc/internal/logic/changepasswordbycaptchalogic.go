682e810420da9c37314218b0eb05dc3e451071a3
hupuqian <663992442@qq.com>
2023-06-01 10:13:45 +0800

    新增user.rpc的修改密码

--- app/user/rpc/internal/logic/changepasswordbycaptchalogic.go
+++ app/user/rpc/internal/logic/changepasswordbycaptchalogic.go
@@ -2,6 +2,10 @@ package logic
 
 import (
 	"context"
+	"giligili/app/captcha/rpc/captchaservice"
+	"giligili/app/user/utils/password"
+	"giligili/common/xerr"
+	"github.com/pkg/errors"
 
 	"giligili/app/user/rpc/internal/svc"
 	"giligili/app/user/rpc/pb"
@@ -24,7 +28,51 @@ func NewChangePasswordByCaptchaLogic(ctx context.Context, svcCtx *svc.ServiceCon
 }
 
 func (l *ChangePasswordByCaptchaLogic) ChangePasswordByCaptcha(in *pb.ChangePasswordByCaptchaRequest) (*pb.Response, error) {
-	// todo: add your logic here and delete this line
+	// 通过captcha rpc服务验证验证码是否正确
+	captcha, err := l.svcCtx.CaptchaRpc.VerifyCaptcha(
+		l.ctx,
+		&captchaservice.VerifyCaptchaReq{
+			Email:   in.Email,
+			Captcha: in.Captcha,
+		},
+	)
+	if err != nil || captcha.Result != 200 {
+		return nil, errors.Wrapf(
+			xerr.NewErrMsg("验证码错误, 请重新输入！"),
+			"verify captcha err: %s", err,
+		)
+	}
 
-	return &pb.Response{}, nil
+	// 查询用户是否存在
+	email, err := l.svcCtx.UserModel.FindOneByEmail(l.ctx, in.Email)
+	if err != nil {
+		return nil, errors.Wrapf(
+			xerr.NewErrMsg("根据邮箱查询用户失败, 请重新修改！"),
+			"find user by email err: %s", err,
+		)
+	}
+
+	// 生成密码
+	generatePassword, err := password.GeneratePassword(in.NewPassword)
+	if err != nil {
+		return nil, errors.Wrapf(
+			xerr.NewErrMsg("密码加密失败, 请重新修改！"),
+			"generate password err: %s", err,
+		)
+	}
+
+	// 修改密码
+	email.Password = generatePassword
+
+	// 更新用户
+	if err = l.svcCtx.UserModel.Update(l.ctx, email); err != nil {
+		return nil, errors.Wrapf(
+			xerr.NewErrMsg("修改密码失败, 请重新修改！"),
+			"update user err: %s", err,
+		)
+	}
+
+	return &pb.Response{
+		Result: 200,
+	}, nil
 }
--- app/user/rpc/internal/logic/changepasswordbypasswordlogic.go
+++ app/user/rpc/internal/logic/changepasswordbypasswordlogic.go
@@ -2,6 +2,9 @@ package logic
 
 import (
 	"context"
+	"giligili/app/user/utils/password"
+	"giligili/common/xerr"
+	"github.com/pkg/errors"
 
 	"giligili/app/user/rpc/internal/svc"
 	"giligili/app/user/rpc/pb"
@@ -24,7 +27,44 @@ func NewChangePasswordByPasswordLogic(ctx context.Context, svcCtx *svc.ServiceCo
 }
 
 func (l *ChangePasswordByPasswordLogic) ChangePasswordByPassword(in *pb.ChangePasswordByPasswordRequest) (*pb.Response, error) {
-	// todo: add your logic here and delete this line
+	// 查询用户是否存在
+	email, err := l.svcCtx.UserModel.FindOneByEmail(l.ctx, in.Email)
+	if err != nil {
+		return nil, errors.Wrapf(
+			xerr.NewErrMsg("根据邮箱查询用户失败, 请重新修改！"),
+			"find user by email err: %s", err,
+		)
+	}
 
-	return &pb.Response{}, nil
+	// 验证原密码是否正确
+	if err = password.ComparePassword(in.OrdPassword, email.Password); err != nil {
+		return nil, errors.Wrapf(
+			xerr.NewErrMsg("原密码错误, 请重新输入！"),
+			"compare password err: %s", err,
+		)
+	}
+
+	// 生成密码
+	generatePassword, err := password.GeneratePassword(in.NewPassword)
+	if err != nil {
+		return nil, errors.Wrapf(
+			xerr.NewErrMsg("密码加密失败, 请重新修改！"),
+			"generate password err: %s", err,
+		)
+	}
+
+	// 修改密码
+	email.Password = generatePassword
+
+	// 更新用户
+	if err = l.svcCtx.UserModel.Update(l.ctx, email); err != nil {
+		return nil, errors.Wrapf(
+			xerr.NewErrMsg("修改密码失败, 请重新修改！"),
+			"update user err: %s", err,
+		)
+	}
+
+	return &pb.Response{
+		Result: 200,
+	}, nil
 }
