fbace07655c16beeff9065ddf3bbb97aba1653cc
syk <663992442@qq.com>
2023-07-11 17:13:14 +0800

    新增ffmpeg的shell脚本 和 工具

--- common/result/httpResult.go
+++ common/result/httpResult.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
-	"looklook/common/xerr"
+	"giligili/common/xerr"
 
 	"github.com/pkg/errors"
 	"github.com/zeromicro/go-zero/core/logx"
--- common/result/jobResult.go
+++ common/result/jobResult.go
@@ -3,7 +3,7 @@ package result
 import (
 	"context"
 
-	"looklook/common/xerr"
+	"giligili/common/xerr"
 
 	"github.com/pkg/errors"
 	"github.com/zeromicro/go-zero/core/logx"
